7b6b1a4df804ec5628b51300b07c25bf7cb5b8f9
anwisniewska <anetawisniewska@poczta.onet.pl>
2018-03-19 10:48:32 +0100

    ogarniecie kodu

--- CPS/CPS/HistogramViewModel.cs
+++ CPS/CPS/HistogramViewModel.cs
@@ -14,25 +14,16 @@ namespace CPS
     {
         public HistogramViewModel()
         {
-            // Create some data
-            //this.Items = new Collection<Item>
-            //                {
-            //                    new Item {Label = "Apples", Value = 37},
-            //                    new Item {Label = "Pears", Value = 7},
-            //                    new Item {Label = "Bananas", Value = 23}
-            //                };
+
         }
 
         public void Make()
         {
-            // Create the plot model
             var tmp = new PlotModel { Title = "Histogram" };
 
-            // Add the axes, note that MinimumPadding and AbsoluteMinimum should be set on the value axis.
             tmp.Axes.Add(new CategoryAxis { ItemsSource = this.Items, LabelField = "Label" });
             tmp.Axes.Add(new LinearAxis { Position = AxisPosition.Left, MinimumPadding = 0, AbsoluteMinimum = 0 });
 
-            // Add the series, note that the BarSeries are using the same ItemsSource as the CategoryAxis.
             tmp.Series.Add(new ColumnSeries { ItemsSource = this.Items, ValueField = "Value" });
 
             this.Model = tmp;
